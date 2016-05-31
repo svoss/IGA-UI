2c74b69f29d8801e1089fd9620626d2968a62412
Kevin Jacobs <kevin91nl@gmail.com>
2016-05-31 11:30:47 +0200

    Tuxx implementation.

--- model.py
+++ model.py
@@ -1,11 +1,34 @@
 import pytz
 
+tuxx = {
+    'monthnames': {
+        'normal': '',
+        'blue': '$(".month-heading").css("color", "#22628b");'
+    },
+    'holiday': {
+        'normal': '',
+        'underlined': '$(".square").css("border", "1px solid white"); $(".square").css("border-bottom", "1px solid #22628b").css("color", "#22628b");',
+    },
+    'billboard': {
+        'normal': '',
+        'disabled': '$(".billboard").remove();'
+    },
+    'weekenddays': {
+        'normal': '',
+        'grey': '$(".mnth-cntr .dys-rw:nth-child(8) .wdth-vdr").css("color", "grey"); $(".mnth-cntr .dys-rw:nth-child(9) .wdth-vdr").css("color", "grey");'
+    },
+    'weeknumbers': {
+        'normal': '',
+        'underlined': '$(".fnt-itlc-bld").css("text-decoration", "underline"); $(".fnt-itlc-bld:nth-child(1)").css("text-decoration", "none");'
+    }
+}
+
 FV = {
     'labels': {
         'normal': '',
         'red': 'var labels=document.getElementsByClassName("label"); for(var i =0; i < labels.length; i++){var s = labels[i].style; s.color="white"; s.background="#e51709"; s.textShadow="none";} ',
         'green': 'var labels=document.getElementsByClassName("label"); for(var i =0; i < labels.length; i++){var s = labels[i].style; s.color="white"; s.background="#0dbd16"; s.textShadow="none";}',
-        'grey':'var labels=document.getElementsByClassName("label"); for(var i =0; i < labels.length; i++){var s = labels[i].style; s.color="white"; s.background="#999999"; s.textShadow="none";}'
+        'grey': 'var labels=document.getElementsByClassName("label"); for(var i =0; i < labels.length; i++){var s = labels[i].style; s.color="white"; s.background="#999999"; s.textShadow="none";}'
     },
     'genres': {
         'normal': '',
@@ -23,24 +46,24 @@ FV = {
         'up': "$('li.genrelinks').after($('strong:contains(\"Acteurs\")').parent());"
     },
     'filmpagina-knop': {
-        'normal':'',
-        'larger':"$('.filmpagina-knop').css('font-size','13px');",
-        'green':"$('.filmpagina-knop').css('background','#0dbd16').css('textShadow','none').css('border','none').css('box-shadow','none').css('font-size','13px');"
+        'normal': '',
+        'larger': "$('.filmpagina-knop').css('font-size','13px');",
+        'green': "$('.filmpagina-knop').css('background','#0dbd16').css('textShadow','none').css('border','none').css('box-shadow','none').css('font-size','13px');"
     },
     'watchlist': {
         'normal': '',
-        'up':"$('div.filmpagina-info-cover div.img').before($('div.filmpagina-info-cover div.watchlist')[0]);"
+        'up': "$('div.filmpagina-info-cover div.img').before($('div.filmpagina-info-cover div.watchlist')[0]);"
     },
-    'tabs-positioning-3':{
-        'normal':'',
-        'netflix':"$('li.filmpagina-tabs-netflix').insertAfter($('li.filmpagina-tabs-cast'));",
-        'kopen':"$('li.filmpagina-tabs-kopen').insertAfter($('li.filmpagina-tabs-cast'));",
-        'tv-gemist':"$('li.filmpagina-tabs-eerderoptv').insertAfter($('li.filmpagina-tabs-cast'));"
+    'tabs-positioning-3': {
+        'normal': '',
+        'netflix': "$('li.filmpagina-tabs-netflix').insertAfter($('li.filmpagina-tabs-cast'));",
+        'kopen': "$('li.filmpagina-tabs-kopen').insertAfter($('li.filmpagina-tabs-cast'));",
+        'tv-gemist': "$('li.filmpagina-tabs-eerderoptv').insertAfter($('li.filmpagina-tabs-cast'));"
 
     },
-    'netflix':{
-        'normal':'',
-        'red':"$('li.filmpagina-tabs-netflix a').css('background-color','#e51709');"
+    'netflix': {
+        'normal': '',
+        'red': "$('li.filmpagina-tabs-netflix a').css('background-color','#e51709');"
     }
 }
 
@@ -63,7 +86,8 @@ example = {
 
 models = {
     'example': example,
-    'FV': FV
+    'FV': FV,
+    'tuxx': tuxx
 }
 
 settings = {
@@ -76,7 +100,7 @@ settings = {
         'ga_profile': '120409781',
         'time_zone': pytz.timezone('Europe/Amsterdam'),
         'prefix': '/example/',
-        'example_url':'http://iga.stijnvoss.com/',
+        'example_url': 'http://iga.stijnvoss.com/',
         's3_bucket': 'iga-example-stijnvoss2',
         's3_region': 'us-east-1',
         'search_algorithm': 'BF'
@@ -84,15 +108,29 @@ settings = {
     'FV': {
         'start_code': "0-0-2-2-2-0-2-0",
         'time_zone': pytz.timezone('Europe/Amsterdam'),
-        's3_bucket':'iga-fv-stijnvoss',
-        's3_region':'us-east-1',
+        's3_bucket': 'iga-fv-stijnvoss',
+        's3_region': 'us-east-1',
         'evaluation_time_per_individual': .4,  # time need per individual to determine fitness in days
         'population_size': 5,
         'ga_account': '5318270',
         'ga_property': 'UA-5318270-1',
         'ga_profile': '120892623',
         'prefix': 'http://cdn-iga.stijnvoss.com/FV/',
-        'example_url':'http://www.filmvandaag.nl/film/1633-the-hitchhikers-guide-to-the-galaxy',
+        'example_url': 'http://www.filmvandaag.nl/film/1633-the-hitchhikers-guide-to-the-galaxy',
+        'search_algorithm': 'IGA'
+    },
+    'tuxx': {
+        'start_code': "0-0-0-0-0",
+        'time_zone': pytz.timezone('Europe/Amsterdam'),
+        's3_bucket': 'iga-tuxx-kevin91nl',
+        's3_region': 'us-east-1',
+        'evaluation_time_per_individual': .4,  # time need per individual to determine fitness in days
+        'population_size': 5,
+        'ga_account': '1509805',
+        'ga_property': 'UA-1509805-2',
+        'ga_profile': '5259836',
+        'prefix': 'http://cdn-iga.stijnvoss.com/tuxx/',
+        'example_url': 'http://www.tuxx.nl/kalenders/a4formaat/2016/',
         'search_algorithm': 'IGA'
     }
 }
